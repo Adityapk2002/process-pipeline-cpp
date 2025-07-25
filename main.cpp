5a234826d98ce457b195ceb93b604003e22c9b70
aditya khandade <adityakhandade2002@gmail.com>
2025-07-26 00:00:33 +0530

    Added comments

--- main.cpp
+++ main.cpp
@@ -6,21 +6,23 @@ void pipeline(const char *process1, const char *process2)
     int fd[2];
     pipe(fd);
     int id = fork();
-    if (id != 0)
+    if (id != 0) // This is the parent process (which will run cat).
     {
-        close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[1]);
+        close(fd[0]);               // close the read end of the pipe
+        dup2(fd[1], STDOUT_FILENO); // redirect standard output to write end of pipe
+        close(fd[1]);               // close the write end of the pipe
 
+        // [execute process]
         execlp("/bin/cat", "cat", "main.cpp", nullptr);
         std::cerr << "Failed to execute" << process1 << std::endl;
     }
-    else
+    else // This is the child process (which will run grep hello).
     {
-        close(fd[1]);
-        dup2(fd[0], STDIN_FILENO);
-        close(fd[0]);
+        close(fd[1]);              // close the write end of the pipe
+        dup2(fd[0], STDIN_FILENO); // redirect standard input to the read end of the pipe
+        close(fd[0]);              // close the read end of the pipe
 
+        //[execute process2]
         execlp("/usr/bin/grep", "grep", "hello", nullptr);
         std::cerr << "Failed to execute " << process2 << std::endl;
     }
